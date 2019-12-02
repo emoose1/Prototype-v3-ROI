ff90e7aea7df8fd119a7bd21e2b9a03c96b3fbdc
muse-ghtechlabs <emmanuel.muse@ghtechlabs.com>
2019-12-02 17:21:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -89,18 +89,6 @@ function display(){
 }
 
 
-//Slider Functions
-var ratings = document.querySelector('#ratings');
-  ratings.addEventListener('value-change', function() {
-    document.querySelector('#ratingsLabel').textContent = ratings.value;
-  });
-
-  var grade = document.querySelector('#grade');
-  grade.addEventListener('value-change', function() {
-    var label = (grade.value < grade.secondaryProgress) ? "Fail" : "Pass" ;
-    document.querySelector('#gradeLabel').textContent = grade.value + " (" + label + ")";
-  });
-
 //For getting the next range sibling 
 function getNextRangeSibling(n)
 {
